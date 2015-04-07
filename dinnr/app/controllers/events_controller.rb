5eeb6f14ac462a62a304743b18e444fd93931ffa
Kate <katebeavis84@gmail.com>
2015-04-07 14:20:54 +0100

    Can add a new event

--- dinnr/app/controllers/events_controller.rb
+++ dinnr/app/controllers/events_controller.rb
@@ -4,4 +4,16 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def new
+  end
+
+  def create
+    Event.create(event_params)
+    redirect_to '/events'
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :date, :size)
+  end
+
 end
--- dinnr/spec/features/events_features_spec.rb
+++ dinnr/spec/features/events_features_spec.rb
@@ -21,4 +21,20 @@ feature 'events' do
     end
   end
 
+  context 'adding an event' do
+
+    scenario 'prompts a user to fill out a from and then displays the new event' do
+      visit '/events'
+      click_link 'Add an event'
+      fill_in 'Title', with: 'Dinner with Thomas'
+      fill_in 'Description', with: "Dinner at Thomas' house"
+      fill_in 'Location', with: 'E1 1EJ'
+      fill_in 'Date', with: 'Tuesday 7.30pm'
+      fill_in 'Size', with: '3'
+      click_button 'Create Event'
+      expect(page).to have_content 'Dinner with Thomas'
+      expect(current_path).to eq '/events'
+    end
+  end
+
 end
