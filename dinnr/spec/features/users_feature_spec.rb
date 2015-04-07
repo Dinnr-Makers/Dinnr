2d6b4e3cec31e07c6c7d70761546455f0392f82e
wardymate <chriswarddev@gmail.com>
2015-04-07 15:31:42 +0100

    sign in and sign up buttons are displayed appropriately on the homepage

--- dinnr/spec/features/users_feature_spec.rb
+++ dinnr/spec/features/users_feature_spec.rb
@@ -3,11 +3,41 @@ require 'rails_helper'
 feature 'users' do
   context 'user not signed in and on he home page' do
 
-    scenario 'should see a sign in and sign up link' do
+    it 'should see a sign in and sign up link' do
       visit '/'
       expect(page).to have_content("Sign in")
       expect(page).to have_content("Sign up")
     end
+
+    it 'should not see a sign out link' do
+      visit '/'
+      expect(page).not_to have_link('Sign out')
+    end
+
+  end
+
+  context 'user signed in and on the home page' do
+
+    before do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
+
+    it 'should see a sign out link' do
+      visit '/'
+      expect(page).to have_link('Sign out')
+    end
+
+    it 'should not have a sign in or sr sign up link' do
+      visit '/'
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+
   end
 
 end
