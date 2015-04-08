1b2dfffb2d37bcd04ae95f5ba9e49326eb8389e7
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-08 12:18:58 +0100

    Added require bower_rails initializer to application rb

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 
 module Dinnr
   class Application < Rails::Application
-    
+    require "#{Rails.root}/config/initializers/bower_rails.rb"
     
 
     # Settings in config/environments/* take precedence over those specified here.
