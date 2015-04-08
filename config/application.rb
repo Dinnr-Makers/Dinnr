0d320a48b13dc00f46c9f77ff6b9e691ce78d120
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-08 12:13:36 +0100

    Removed bootstrap from application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,9 @@ Bundler.require(*Rails.groups)
 
 module Dinnr
   class Application < Rails::Application
+    
+    
 
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-  
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -38,3 +36,4 @@ module Dinnr
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
