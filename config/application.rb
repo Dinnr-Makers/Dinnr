b6abdc81c4d8cbb5d527c5d573ff5d5663a10fa6
Sean Haughton <seanphaughton@gmail.com>
2015-04-08 16:56:35 +0100

    Refactored asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Dinnr
    
    
     config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
-    config.assets.paths << Rails.root.join("assets", "font")
+    config.assets.paths << Rails.root.join("app", "assets", "font")
     config.assets.precompile << %r(.*.(?:eot|svp|ttf|woff|woff2)$)
 
     # Settings in config/environments/* take precedence over those specified here.
