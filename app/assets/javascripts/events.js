f3e35d4ce22b1c691d4d70e94e705fbb0df98c13
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-09 15:22:26 +0100

    Transformed location form into rails form and spit put the data into the show events view.

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -22,7 +22,6 @@ var componentForm = {
   street_number: 'short_name',
   route: 'long_name',
   locality: 'long_name',
-  administrative_area_level_1: 'short_name',
   country: 'long_name',
   postal_code: 'short_name'
 };
