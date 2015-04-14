4527d872036fc13ea23781a860a8000df07038b1
wardymate <chriswarddev@gmail.com>
2015-04-14 08:40:10 +0100

    review rating can only be 1-10

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
 
   belongs_to :event
+  validates :rating, inclusion: (1..10)
 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,12 @@ describe Review, type: :model do
 
   it { is_expected.to belong_to :event }
 
+  describe Review, :type => :model do
+  it "is invalid if the rating is more than 10" do
+    review = Review.new(rating: 15)
+    expect(review).to have(1).error_on(:rating)
+  end
+
+end
+
 end
