d417db4c368b7f6bb06512342e08595cbd816479
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-16 00:33:31 +0100

    New map style.

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -53,7 +53,8 @@ $(document).ready( function() {
 function initializeMainMap(json) {
   map = new google.maps.Map(document.getElementById("main-map-canvas"), {
     zoom: 12,
-    center: new google.maps.LatLng(json.features[0].geometry.coordinates[1], json.features[0].geometry.coordinates[0])
+    center: new google.maps.LatLng(json.features[0].geometry.coordinates[1], json.features[0].geometry.coordinates[0]),
+    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
   });
 
 
