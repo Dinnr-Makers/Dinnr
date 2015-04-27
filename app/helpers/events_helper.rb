6b69cd2219d10a843bebdca6cac877d434884d29
wardymate <chriswarddev@gmail.com>
2015-04-27 16:06:22 +0100

    refactored events helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,12 @@
 module EventsHelper
 
   def get_guests_and_pictures
-    @guests = Booking.where("event_id = #{@event.id}").map{|booking| booking.user_id}.map{|guest| User.find(guest)}
-    @pictures = Eventpicture.where("event_id = #{@event.id}").map{|ep| ep.picture_id}.map{|picture| Picture.find(picture)}
+    @guests = Booking.where(event_extras).map{|booking| booking.user_id}.map{|guest| User.find(guest)}
+    @pictures = Eventpicture.where(event_extras).map{|ep| ep.picture_id}.map{|picture| Picture.find(picture)}
   end
+
+  def event_extras
+    "event_id = #{@event.id}"
+  end
+
 end
